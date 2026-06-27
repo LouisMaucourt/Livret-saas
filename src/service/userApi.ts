437f3f5f1072e116588ea364eaa268949e0b7e79
LouisMaucourt <louis.maucourt7@gmail.com>
2026-06-27 20:48:49 +0200

    ahhhhhhh

--- src/service/userApi.ts
+++ src/service/userApi.ts
@@ -4,54 +4,54 @@ export const infosApi = async (id: string | undefined) => {
     return res.json()
 }
 export const userApi = async () => {
-    const res = await fetch("http://localhost:3000/api/users")
+    const res = await fetch("/api/users")
     return res.json()
 }
 export const wifiApi = async (id: string | undefined) => {
-    const res = await fetch(`http://localhost:3000/api/wifi?id=${id}`)
+    const res = await fetch(`/api/wifi?id=${id}`)
     return res.json()
 }
 export const stayDetailsApi = async (id: string | undefined) => {
-    const res = await fetch(`http://localhost:3000/api/staydetails?id=${id}`)
+    const res = await fetch(`/api/staydetails?id=${id}`)
     return res.json()
 }
 export const getRulesApi = async (id: string | undefined) => {
-    const res = await fetch(`http://localhost:3000/api/rules?id=${id}`)
+    const res = await fetch(`/api/rules?id=${id}`)
     return res.json()
 }
 export const getContactApi = async (id: string | undefined) => {
-    const res = await fetch(`http://localhost:3000/api/contact?id=${id}`)
+    const res = await fetch(`/api/contact?id=${id}`)
     return res.json()
 }
 export const getPlacesApi = async (id: string | undefined) => {
-    const res = await fetch(`http://localhost:3000/api/places?id=${id}`)
+    const res = await fetch(`/api/places?id=${id}`)
     return res.json()
 }
 export const getSectionApi = async (id: string | undefined) => {
-    const res = await fetch(`http://localhost:3000/api/sections?id=${id}`)
+    const res = await fetch(`/api/sections?id=${id}`)
     return res.json()
 }
 export const checkinAllApi = async () => {
-    const res = await fetch(`http://localhost:3000/api/checkinAll`)
+    const res = await fetch(`/api/checkinAll`)
     return res.json()
 }
 export const propertiesApi = async () => {
-    const res = await fetch("http://localhost:3000/api/properties")
+    const res = await fetch("/api/properties")
     return res.json()
 }
 export const BookletsApi = async (id: string | undefined) => {
-    const res = await fetch(`http://localhost:3000/api/booklets?id=${id}`)
+    const res = await fetch(`/api/booklets?id=${id}`)
     return res.json()
 }
 export const BookletApi = async (id: string | undefined) => {
-    const res = await fetch(`http://localhost:3000/api/booklet?id=${id}`)
+    const res = await fetch(`/api/booklet?id=${id}`)
     return res.json()
 }
 export const QrCodeApi = async (id: string | undefined) => {
-    const res = await fetch(`http://localhost:3000/api/qrcode?id=${id}`)
+    const res = await fetch(`/api/qrcode?id=${id}`)
     return res.json()
 }
 export const getClientsApi = async (id: string | undefined) => {
-    const res = await fetch(`http://localhost:3000/api/clients?id=${id}`)
+    const res = await fetch(`/api/clients?id=${id}`)
     return res.json()
 }
